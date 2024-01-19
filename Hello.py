14548b6f1b82d10d7d9c096481a41afad29d48f8
jiro <johnjiro1114@gmail.com>
2024-01-19 22:53:35 +0900

    Hopeページを初期テンプレートの状態から変更

--- Hello.py
+++ Hello.py
@@ -24,26 +24,17 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# デモアプリです")
 
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+        Noteのトレンド記事をAPIから取得し、
+        分析してみました.
+        
+        menuから選択してください。
+        """
     )
 
 
