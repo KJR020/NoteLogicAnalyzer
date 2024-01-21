bdf5d2eba8b91cb41bfd1d2826275ca056ce6aed
jiro <johnjiro1114@gmail.com>
2024-01-21 23:37:06 +0900

    Pytestの動作確認 True, False をassertして期待どおりの反応するかチェック

---
+++ tests/test_article_data_processer.py
@@ -0,0 +1,11 @@
+import pytest
+
+from services.article_data_processer import fetch_trend_articles
+
+
+class TestArticleDataProcesser:
+    def test_動作チェック(self):
+        assert False
+
+    def test_動作チェック2(self):
+        assert True
