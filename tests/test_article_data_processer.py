482b17ea4a74c67b2553a277fed77cd27fb0371b
jiro <johnjiro1114@gmail.com>
2024-01-24 22:47:39 +0900

    引数のselfが抜けていたため修正

--- tests/test_article_data_processer.py
+++ tests/test_article_data_processer.py
@@ -7,7 +7,9 @@ class TestNoteApiDaFetcher:
     def test_動作チェック(self):
         assert True
 
-    def test_apiから記事データを取得できる():
-        note_api_data_fetcher = NoteApiDataFetcher("https://note.com/api/v3/notes")
+    def test_APIから記事データを取得できる(self):
+        note_api_data_fetcher = NoteApiDataFetcher(
+            api_url="https://note.com/api/v3/notes"
+        )
         df = note_api_data_fetcher.fetch_data()
         assert df is not None
